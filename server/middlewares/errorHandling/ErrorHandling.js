89a548bb58bdf02bebc47c71676eb7ece76bc2c6
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2023-01-08 20:56:07 +0000

    edit in  error handler middleware

--- server/middlewares/errorHandling/ErrorHandling.js
+++ server/middlewares/errorHandling/ErrorHandling.js
@@ -2,8 +2,6 @@ const ErrorHandler = (err, req, res, next) => {
   const errStatus = err.statusCode || 500;
   const errMsg = err.message || 'Something went wrong';
   res.status(errStatus).json({
-      success: false,
-      status: errStatus,
       message: errMsg,
       stack: err.stack
   })
